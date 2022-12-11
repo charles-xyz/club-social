6488a1795d4c31d031c1cbd3e253bf5db86ec981
dirty' don <donnieWalsh@users.noreply.github.com>
2022-12-11 11:33:57 -0500

    Initial Commit

---
+++ beta/betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  betaApp.swift
+//  beta
+//
+//  Created by Charles Lazaroni on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
